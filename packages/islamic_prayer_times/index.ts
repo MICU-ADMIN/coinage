2a97a0edca90b1c61ffcd0e254017ed8e0532aaa
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-28 20:19:24 +0100

    git

--- packages/islamic_prayer_times/index.ts
+++
@@ -1,7 +0,0 @@
-import { getPrayerTimeMethods } from './func/api/getMethods';
-import { getPrayerTimes } from './func/api/getTimes';
-
-export default {
-	getPrayerTimes,
-	getPrayerTimeMethods,
-};
--- packages/producer_micu/util/maths.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomNumber = void 0;
-function generateRandomNumber(minValue, maxValue) {
-    // Generate a random number between minValue (inclusive) and maxValue (inclusive)
-    return (Math.floor(Math.random() *
-        (maxValue - minValue + 1)) + minValue);
-}
-exports.generateRandomNumber = generateRandomNumber;
