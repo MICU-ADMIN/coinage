61ca813b00e7f72b322143674bebdc3e6fb9546d
Mitch <Mitch.Ngugi99@gmail.com>
2023-11-07 23:35:19 +0000

    ui skelly part 1

--- apps/isnad2/apps/next/pages/index.tsx
+++ apps/isnad2/apps/next/pages/index.tsx
@@ -1,5 +1,7 @@
 import { HomeScreen } from 'app/features/home/screen'
 import Head from 'next/head'
+import LeftSideBar from './components/leftSidebar/LeftSideBar';
+import HadithView from './components/hadithView/HadithView';
 
 export default function Page() {
   return (
@@ -8,6 +10,8 @@ export default function Page() {
         <title>Home</title>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </Head>
+      <LeftSideBar/>
+      <HadithView />
       <HomeScreen />
     </>
   )
